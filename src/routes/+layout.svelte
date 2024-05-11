67edcfcb3cd87554d089c10d123f7be878ae2a9e
David <davidstrasak@seznam.cz>
2024-05-11 16:08:21 +0200

    add tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<h1 class="text-green-500">Header</h1>
+<slot />
+<button class="btn"></button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { base } from '$app/paths';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-4xl font-bold underlin text-green-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <a href="{base}/about">About me</a>
