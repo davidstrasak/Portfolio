80281021e9413788ff32e8b3cc261c279dac3b20
David <davidstrasak@seznam.cz>
2024-05-11 15:25:12 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="{base}/about">About me</a>
