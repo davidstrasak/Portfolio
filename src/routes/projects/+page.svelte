32ced4d67e38f7540ee9be735539a59699a70500
David <davidstrasak@seznam.cz>
2024-05-12 00:53:28 +0200

    add stuff to footer

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,3 +3,39 @@
 	<strong class="inline text-secondary">projects</strong>
 </h1>
 <p>jdksljlk</p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, aliquid! Laborum expedita non beatae
+ipsam. Libero cumque natus, id sint, dolorem atque similique asperiores delectus beatae possimus enim
+totam quam! Dignissimos pariatur omnis quod voluptatibus? Dolore vel ex corporis minus, libero nam! Corporis
+quia quo natus blanditiis necessitatibus, non totam impedit vero adipisci, ex dolore, cum molestias labore
+accusamus velit hic tenetur recusandae possimus dignissimos ea architecto. Nostrum laborum ullam, corporis
+nobis saepe maxime porro adipisci ipsum. Tempora voluptas enim earum fugit voluptate, at porro ipsum
+optio itaque quasi possimus alias facere quas beatae consectetur, doloremque laborum eum provident quisquam
+numquam veritatis. Facere quae, veritatis fuga quo asperiores cum laudantium corporis, unde qui rem quidem,
+dignissimos impedit deserunt modi soluta molestiae repellendus eos dolore dolores et quam dolorem consequuntur
+perspiciatis. Asperiores neque accusantium eius quos dicta rerum facere, cum a consectetur error omnis
+optio eos deserunt consequuntur fugiat nisi facilis, nesciunt esse? Sint ut voluptas fuga debitis quod
+saepe, nulla totam repellendus neque vero aliquam sed et reiciendis doloremque numquam officiis architecto,
+voluptatum autem expedita dolorem? Cum cupiditate ipsa eius quia quo unde iste deleniti eligendi delectus
+labore nemo rem, voluptas tempore et! Sunt molestias exercitationem fuga ea quae, maiores consequatur
+rem voluptate maxime aut excepturi facilis necessitatibus architecto atque sequi, ipsam vel obcaecati
+pariatur, ut doloribus officia! Cumque quod illo magnam voluptates, fugit laudantium! Id autem voluptatum,
+laborum, magnam a vel molestiae blanditiis accusamus aut est harum illo vitae nesciunt totam praesentium
+dolorem tempore aliquam cupiditate sint quibusdam dolore quaerat sunt magni. Cum sapiente excepturi at,
+magnam repudiandae molestiae possimus magni voluptatem. Nemo perferendis libero tenetur enim unde neque!
+Perspiciatis voluptatibus quas, et quaerat consequatur quasi rerum est excepturi consequuntur facere
+sed iure obcaecati vero totam ad illo quae voluptatum? Laborum suscipit quibusdam assumenda architecto
+expedita culpa officia autem. Voluptatum, ab, fugiat itaque sapiente, possimus enim dolorum fugit maiores
+tenetur quas mollitia dolor. Aut aperiam explicabo earum expedita et placeat, neque natus sit eos consequuntur
+quo. Dolor voluptatibus commodi possimus quam ducimus laborum eaque consectetur mollitia beatae porro,
+animi blanditiis consequuntur, harum facere culpa eos nihil optio corporis ipsam ea, nobis minus. Provident
+voluptatum at distinctio optio culpa, iusto labore quaerat nihil soluta mollitia magni? Magnam laborum
+quia in hic odio facilis laboriosam sunt. Molestiae, itaque? Assumenda iste sed quaerat aut iusto sint
+velit quae sapiente libero, ipsum quo nulla, magnam fugiat fuga commodi! Nam, fuga! Delectus, asperiores
+at? Aperiam ipsa est perferendis ex provident nam laboriosam quibusdam, necessitatibus, quis culpa blanditiis
+recusandae temporibus eum officiis, natus illo esse saepe hic! Aliquam ut, quas incidunt sit delectus,
+consectetur animi suscipit quaerat cum doloremque in iusto eaque neque eius perferendis laudantium perspiciatis
+alias similique cumque optio repellat possimus. Cupiditate sed itaque odio animi assumenda, necessitatibus
+dolores error, maiores ab facere facilis possimus deserunt, perspiciatis ducimus quia similique modi?
+Eius consequatur eveniet quae quam alias optio repudiandae iure dolores maxime deserunt exercitationem
+consectetur ex dignissimos assumenda inventore, placeat animi laboriosam cum quia? Porro sed dolorem
+dolorum accusamus ex, dicta magni in.
