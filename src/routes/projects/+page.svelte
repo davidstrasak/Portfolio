1a98e924969fe9bc59367c4da6a20bb3522001ff
David <davidstrasak@seznam.cz>
2024-05-12 01:23:13 +0200

    add scrollbar styling and modal window to the first  project

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -16,7 +16,39 @@
 			<h2 class="card-title">Shoes!</h2>
 			<p>If a dog chews shoes whose shoes does he choose?</p>
 			<div class="card-actions justify-end">
-				<button class="btn btn-primary">Buy Now</button>
+				<!-- The button to open modal -->
+				<label for="my_modal_7" class="btn">open modal</label>
+
+				<!-- Put this part before </body> tag -->
+				<input type="checkbox" id="my_modal_7" class="modal-toggle" />
+				<div class="modal" role="dialog">
+					<div class="modal-box">
+						<h3 class="text-lg font-bold">Hello!</h3>
+						<p class="py-4">This modal works with a hidden checkbox!</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam perspiciatis
+							praesentium incidunt, officiis deleniti ratione quidem blanditiis facere. Ratione
+							tempore maiores error qui. Eius, fugiat nesciunt. Cum deserunt necessitatibus, nam
+							iure officiis enim culpa at labore quos explicabo laborum repellat, quae corporis
+							consectetur nostrum similique quo vel a laudantium perspiciatis! Voluptas ipsum
+							maiores mollitia! Quod, vero recusandae. Vitae laboriosam inventore cupiditate?
+							Perferendis sunt officiis optio quisquam eos expedita sapiente quaerat laudantium aut
+							et numquam, nobis, nemo qui. Odio reprehenderit aut eaque maxime tenetur id pariatur
+							commodi fuga aperiam amet culpa quia voluptatibus omnis eum et dignissimos nam
+							voluptas necessitatibus nostrum vel, nulla expedita autem? Eligendi explicabo quas
+							sapiente culpa ipsam, cupiditate minima iusto impedit error, pariatur, nam aliquam
+							modi. Nobis maxime officia saepe, rerum quod laboriosam, aliquid cum consequatur
+							sapiente corrupti, quibusdam a. Sint veritatis odit quis officia, dicta officiis id
+							fuga laboriosam ut voluptatum nam optio debitis dolorem doloribus laudantium incidunt,
+							sequi obcaecati totam. Iste est molestias optio minus aliquam veritatis ullam.
+							Corrupti ut at, perspiciatis quasi ullam cupiditate culpa nam incidunt quis
+							voluptatem, numquam veritatis? Repellat, deserunt! Inventore nam ipsa nesciunt
+							recusandae id accusantium! A voluptate, asperiores temporibus voluptates at odio
+							accusantium! Similique in esse accusantium amet tenetur!
+						</p>
+					</div>
+					<label class="modal-backdrop" for="my_modal_7">Close</label>
+				</div>
 			</div>
 		</div>
 	</div>
