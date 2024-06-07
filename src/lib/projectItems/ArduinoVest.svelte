8f2b42cc6019c4e96f3076bf21505111338d66c5
David <davidstrasak@seznam.cz>
2024-06-07 13:00:49 +0200

    add arduinovest page

--- src/lib/projectItems/ArduinoVest.svelte
+++ src/lib/projectItems/ArduinoVest.svelte
@@ -2,32 +2,35 @@
 	import { base } from "$app/paths";
 </script>
 
-<img
-	style="float: right"
-	src="{base}/images/Vest_Gif.gif"
-	alt="a vest with LEDs powered by an arduino"
-	width="30%"
-	height="30%"
-/>
 <div>
-	<a class="text-accent" href="https://github.com/davidstrasak/Cybertown_LEDs/" target="_blank"
-		>Code here</a
-	>
+	<div class="block items-center w-full">
+		<img
+			src="{base}/images/Vest_Gif.gif"
+			alt="a vest with LEDs powered by an arduino"
+			width="50%"
+			height="50%"
+			class="block m-auto"
+		/>
+		<a
+			class="text-accent block font-bold text-left"
+			href="https://github.com/davidstrasak/Cybertown_LEDs/"
+			target="_blank">Code here</a
+		>
+	</div>
 
-	<h2>Lessons learned:</h2>
-	<ul>
+	<h2 class="text-secondary font-bold">Lessons learned:</h2>
+	<ol class="ml-10">
 		<li>Working with an arduino LED library.</li>
 		<li>My first big soldering project.</li>
 		<li>Using PWM pins to connect LEDs.</li>
 		<li>Next time I will solder the cables to the arduino. Not superglue them to the pins.</li>
-	</ul>
+	</ol>
 
-	<h2>Description</h2>
-	<p>I've created this vest for the Cybertown event to look like a cyberpunk.</p>
+	<h2 class="text-secondary font-bold">Description</h2>
+	<p>I've created this vest for the Cybertown event to look like a proper cyberpunk.</p>
 	<p>
-		I had no idea what I was doing - first time setting up LEDs to something like this or painting
-		the vest. One thing about being an engineer though, is you realise you can figure stuff out if
-		you think about the process.
+		I had no idea what I was doing - this was my first hardware project ever. One thing about being
+		an engineer though, is you realise you can figure stuff out if you think about the process.
 	</p>
 	<p>
 		The code itself is pretty simple - just initialise the LED pins, set up the animation and run
@@ -41,12 +44,8 @@
 </div>
 
 <style>
-	ul {
-		list-style-type: none; /* Remove default list styling */
-	}
-
-	ul li:before {
-		content: "- "; /* Add a dash before each list item */
+	ol {
+		list-style-type: decimal; /* Remove default list styling */
 	}
 	* {
 		margin-top: 10px;
