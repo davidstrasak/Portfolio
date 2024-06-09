2947c0774df280e22e3a15a38ca7c9fa6b1d04d3
David <davidstrasak@seznam.cz>
2024-06-09 18:23:41 +0200

    add the portal component so modals are rendered properly

---
+++ src/lib/Portal.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	let ref: any;
+	let portal: any;
+
+	onMount(() => {
+		portal = document.createElement("div");
+		portal.className = "portal";
+		document.body.appendChild(portal);
+		portal.appendChild(ref);
+		return () => {
+			document.body.removeChild(portal);
+		};
+	});
+</script>
+
+<div class="portal-clone">
+	<div bind:this={ref}>
+		<slot></slot>
+	</div>
+</div>
+
+<style>
+	.portal-clone {
+		display: none;
+	}
+</style>
--- src/lib/projectStructure/Modal.svelte
+++ src/lib/projectStructure/Modal.svelte
@@ -3,6 +3,8 @@
 	export let id: number = 0;
 	export let title: string = "Default";
 
+	import Portal from "$lib/Portal.svelte";
+
 	import ArduinoVest from "$lib/projectItems/ArduinoVest.svelte";
 	import AutomaticWindow from "$lib/projectItems/AutomaticWindow.svelte";
 	import PICvariety from "$lib/projectItems/PICvariety.svelte";
@@ -21,18 +23,20 @@
 </script>
 
 <!-- Put this part before </body> tag -->
-<input type="checkbox" id="my_modal_{id}" class="modal-toggle" />
-<div class="modal modal-bottom sm:modal-middle" role="dialog">
-	<div class="modal-box" style="width: 1500px;">
-		<h3 class="text-3xl font-bold text-secondary">{@html title}</h3>
+<Portal>
+	<input type="checkbox" id="my_modal_{id}" class="modal-toggle" />
+	<div class="modal modal-bottom sm:modal-middle" role="dialog">
+		<div class="modal-box font-cyberpunk" style="width: 1500px;">
+			<h3 class="text-3xl font-bold text-secondary">{@html title}</h3>
 
-		<p class="text-xl mt-4 text-justify">
-			<!-- Importing the modal components had to be done so unintuitively, because otherwise it would throw me an SSR error. -->
-			<svelte:component this={components[modal]} />
-		</p>
+			<p class="text-xl mt-4 text-justify">
+				<!-- Importing the modal components had to be done so unintuitively, because otherwise it would throw me an SSR error. -->
+				<svelte:component this={components[modal]} />
+			</p>
+		</div>
+		<label class="modal-backdrop" for="my_modal_{id}">Close</label>
 	</div>
-	<label class="modal-backdrop" for="my_modal_{id}">Close</label>
-</div>
+</Portal>
 
 <!-- The button to open modal -->
 <label for="my_modal_{id}" class="btn btn-lg btn-outline"> Read more </label>
