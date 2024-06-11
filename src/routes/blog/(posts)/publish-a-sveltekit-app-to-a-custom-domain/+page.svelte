92a3c7e21fd14f89bbd3e709a79dcce606723717
davidstrasak <davidstrasak@seznam.cz>
2024-06-11 15:51:04 +0200

    add a blog post

---
+++ src/routes/blog/(posts)/publish-a-sveltekit-app-to-a-custom-domain/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    let Haloo = "Haloo";
+</script>
+<div>
+<h1 >Publish a static SvelteKit app to a custom domain on github pages</h1>
+<p>As you might see, I've recently published a static SvelteKit website (this website). </p>
+<p>Which is why I've decided to make this blog post - to document all the steps that you can take in order to have a nice little static SvelteKit website.</p>
+<br/>
+<h2>But first: for the unitiated: What is SvelteKit?</h2>
+<p>It's a web building framework. It allows you to follow a few steps to have websites that are reactive.</p>
+<p>For me personally I love SvelteKit, because it is intuitive and it does not require a lot of hoops to jump through (unlike React). Things just make sense</p>
+</div>
