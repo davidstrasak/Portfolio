704b92f4b78eafedb12c5e4b3b0210d60fbfa821
davidstrasak <davidstrasak@seznam.cz>
2024-06-11 10:46:39 +0200

    Updates

---
+++ _app/immutable/chunks/paths.B_AtXOHF.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_162hq4)==null?void 0:s.base)??"/Portfolio_Sveltekit";var t;const a=((t=globalThis.__sveltekit_162hq4)==null?void 0:t.assets)??e;export{a,e as b};
