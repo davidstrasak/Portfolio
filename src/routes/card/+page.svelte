33e3ec9a799dee546bc3da718aaa63f6c9926dcf
davidstrasak <davidstrasak@seznam.cz>
2024-09-01 15:53:05 +0200

    initialise the card and hide some stuff from the footer on smaller screens so it looks good

--- src/routes/card/+page.svelte
+++ src/routes/card/+page.svelte
@@ -13,7 +13,7 @@
 		</figure>
 		<div class="card-body">
 			<h2 class="card-title">Shoes!</h2>
-			<p>If a dog chews shoes whose shoes does he choose?</p>
+			<p>If a dog chews shoes whose shoes does he choose</p>
 			<div class="card-actions justify-end">
 				<button class="btn btn-primary">Buy Now</button>
 			</div>
