74f5e06119319b5ad65f9e05dc162eb334d2e937
davidstrasak <davidstrasak@seznam.cz>
2024-09-01 15:52:09 +0200

    initialise the card route and fix the layout of the footer a bit

--- src/routes/card/+page.svelte
+++ src/routes/card/+page.svelte
@@ -1,1 +1,22 @@
-
+<div class="text-center justify-center items-center">
+	<div class="avatar">
+		<div class="w-24 rounded-full">
+			<img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+		</div>
+	</div>
+	<div class="card bg-base-100 w-96 shadow-xl">
+		<figure>
+			<img
+				src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+				alt="Shoes"
+			/>
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Shoes!</h2>
+			<p>If a dog chews shoes whose shoes does he choose?</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Buy Now</button>
+			</div>
+		</div>
+	</div>
+</div>
