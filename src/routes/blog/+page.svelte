3d8c865ee2e27a0186afb307ec9551ea29393f40
davidstrasak <davidstrasak@seznam.cz>
2025-02-03 16:02:38 +0100

    add blog SEO tags

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,8 +4,23 @@
 	import posts from "./posts";
 </script>
 
+<svelte:head>
+	<meta property="og:title" content="Systemancer - Articles" />
+	<meta
+		name="description"
+		property="og:description"
+		content="A collection of technical articles and insights on automation and systems engineering."
+	/>
+	<meta
+		name="keywords"
+		property="og:keywords"
+		content="Systemancer, Blog, Articles, Systems Engineering, Engineering"
+	/>
+	<meta property="og:url" content="https://davidstrasak.net/blog" />
+</svelte:head>
+
 <h1 class="block text-primary text-4xl font-systemancer text-center">Recent blog posts</h1>
-<a href="{base}/blog/navigation" class="text-xl text-accent underline text-center"
+<a href="{base}/blog/navigation" class="text-xl text-accent underline text-center block"
 	>See all blog posts here</a
 >
 
