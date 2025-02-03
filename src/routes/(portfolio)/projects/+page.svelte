e838785bf2d8a7d0a8e961ea7257bf83bb9793ec
davidstrasak <davidstrasak@seznam.cz>
2025-02-03 19:49:48 +0100

    put the projects page text into a block

--- src/routes/(portfolio)/projects/+page.svelte
+++ src/routes/(portfolio)/projects/+page.svelte
@@ -4,8 +4,8 @@
 	import projects from "./projects";
 </script>
 
-<h1 class="inline text-4xl text-primary font-systemancer text-center">My recent projects</h1>
-<p class="text-xl font-systemancer text-center">... and the lessons learned from them</p>
+<h1 class="block text-4xl text-primary font-systemancer text-center">My recent projects</h1>
+<p class="text-xl font-systemancer text-center block">... and the lessons learned from them</p>
 
 {#each projects as project, index}
 	<Project {project} reverse={index % 2 === 0} />
