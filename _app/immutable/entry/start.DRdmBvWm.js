ec564b7f5091ace8a758bfde463171dcde796f2f
davidstrasak <davidstrasak@seznam.cz>
2025-02-03 16:15:28 +0100

    Updates

---
+++ _app/immutable/entry/start.DRdmBvWm.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/nMEd20e-.js";export{t as start};
---
+++ _app/immutable/nodes/6.C2lQYgz8.js
@@ -0,0 +1,1 @@
+import{a as t,t as o}from"../chunks/Cy94rl3v.js";import{s as e}from"../chunks/Db2Wpu7f.js";import{b as s}from"../chunks/DNPFnRCg.js";var i=o('<img alt="CV" height="90%" width="90%" class="m-auto border-primary border-solid border-8">');function b(a){var r=i();e(r,"src",`${s}/CV_David_Strasak.jpg`),t(a,r)}export{b as component};
