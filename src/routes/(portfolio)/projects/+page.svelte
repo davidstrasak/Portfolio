cf434278365b0817396c7c8e6ef0783b1b9e9b7c
davidstrasak <davidstrasak@seznam.cz>
2025-02-03 15:15:24 +0100

    add SEO meta tags for google to show the site when searching

--- src/routes/(portfolio)/projects/+page.svelte
+++ src/routes/(portfolio)/projects/+page.svelte
@@ -4,10 +4,8 @@
 	import projects from "./projects";
 </script>
 
-<h1 class="text-center">
-	<p class="inline text-4xl text-primary font-systemancer">My recent projects</p>
-	<p class="text-xl font-systemancer">... and the lessons learned from them</p>
-</h1>
+<h1 class="inline text-4xl text-primary font-systemancer text-center">My recent projects</h1>
+<p class="text-xl font-systemancer text-center">... and the lessons learned from them</p>
 
 {#each projects as project, index}
 	<Project {project} reverse={index % 2 === 0} />
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,10 +4,10 @@
 	import posts from "./posts";
 </script>
 
-<h1 class="text-center">
-	<p class="block text-primary text-4xl font-systemancer">Recent blog posts</p>
-	<a href="{base}/blog/navigation" class="text-xl text-accent underline">See all blog posts here</a>
-</h1>
+<h1 class="block text-primary text-4xl font-systemancer text-center">Recent blog posts</h1>
+<a href="{base}/blog/navigation" class="text-xl text-accent underline text-center"
+	>See all blog posts here</a
+>
 
 {#each posts as post, index}
 	<BlogPost {post} reverse={index % 2 === 0} />
