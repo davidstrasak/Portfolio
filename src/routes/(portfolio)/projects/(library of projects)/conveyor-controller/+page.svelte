1e8515ca5c98efbcde1d300198f47a83f716686d
davidstrasak <davidstrasak@seznam.cz>
2025-04-14 10:25:44 +0200

    add a new project about my bachelors thesis - yet unfinished

---
+++ src/routes/(portfolio)/projects/(library of projects)/conveyor-controller/+page.svelte
@@ -0,0 +1,97 @@
+<script>
+	import { base } from "$app/paths";
+	import { page } from "$app/state";
+	import Project from "$lib/projects/Project.svelte";
+	import projects from "../../projects";
+
+	let currentProject = projects.find(
+		(proj) => proj.demoLink.split("/").pop() === page.url.pathname.split("/").pop()
+	);
+</script>
+
+<Project project={currentProject} insideProject={true} />
+
+<img
+	src="{base}/images/projects/Embedded_System.jpeg"
+	width="40%"
+	height="40%"
+	class="float-right"
+	alt="A board with a bunch of electrical parts "
+/>
+
+<h2 class="text-primary font-systemancer">Github Links:</h2>
+<a href="https://github.com/davidstrasak/Bachelors_code" target="_blank" class="text-accent"
+	>Microcontroller Code</a
+>
+<br />
+<a href="https://github.com/davidstrasak/Bachelors_PhoneApp" target="_blank" class="text-accent"
+	>Phone app Code</a
+>
+<br />
+<a href="https://github.com/davidstrasak/Bachelors_PCB" target="_blank" class="text-accent"
+	>PCB Design files</a
+>
+
+<div class="font-readable">
+	<!-- <h2 class="text-primary font-systemancer">Lessons learned:</h2>
+	<ol class="ml-10">
+		<li>Found a passion for designing PCBs</li>
+		<li>Setting and initialising a ton of bits and pins.</li>
+		<li>
+			Getting to know timers, interrupts, UART, ADC, DAC, sleep modes and how microcontrollers work.
+		</li>
+		<li>And also it was my first experience properly programming in C.</li>
+	</ol>
+	<h2 class="text-primary font-systemancer">Description:</h2>
+	<p>
+		I've been able to take a course on working with embedded systems at school. The main curriculum
+		is <span class="text-secondary"
+			>programming in the C language and programming the PIC18F46K22 processor
+		</span> by MicroChip.
+	</p>
+	<p>
+		The whole concept of low level programming is interesting to me and I will be doing some more
+		when possible.
+	</p>
+	<p>
+		The final project of this course was <span class="text-secondary font-bold"
+			>creating 5 tasks:</span
+		>
+	</p>
+	<ul class="ml-10">
+		<li>UART - the processor takes an input and sends it back reversed</li>
+		<li>PWM - increment the brightness of an LED</li>
+		<li>ADC - display the values of the two potentiometers</li>
+		<li>DAC - generate three signals that I can display on an osciloscope on the PC</li>
+		<li>
+			GAME - program a game where you try to match a random number with the potentiometer value
+		</li>
+		<li class="text-secondary">
+			Menu - program an interface and navigation so that I can switch through the tasks
+		</li>
+	</ul>
+
+	<p>
+		This was probably the biggest coding project I have done in regards to <span
+			class="text-secondary">tracking state inside a project</span
+		>. So if you want to see how I did it, you can check out the code on my GitHub.
+	</p> -->
+</div>
+
+<style>
+	ol {
+		list-style-type: decimal; /* Remove default list styling */
+	}
+	ul {
+		list-style-type: initial; /* Remove default list styling */
+	}
+	p {
+		margin-top: 5%;
+	}
+	h2 {
+		margin-top: 5%;
+	}
+	a {
+		margin-top: 5%;
+	}
+</style>
--- src/routes/(portfolio)/projects/projects.ts
+++ src/routes/(portfolio)/projects/projects.ts
@@ -1,35 +1,41 @@
 import { base } from "$app/paths";
 
 interface Project {
-	completionDate: string;
 	projectTitle: string;
 	overview: string;
 	thumbnail: string;
 	demoLink: string;
 	technologies: string[];
 }
-const vestWithLEDs: Project = {
-	completionDate: "01.01.2020",
-	projectTitle: "Vest with LEDs",
-	overview: `My favourite hardware project. It included six LEDs soldered to cables
-		connected to one Arduino Uno, powered by a 9V battery.`,
-	thumbnail: `${base}/images/projects/Vest_with_LEDs.jpg`,
-	demoLink: `${base}/projects/arduino-vest`,
-	technologies: ["Arduino"]
-};
-
-const automaticWindow: Project = {
-	completionDate: "02.01.2020",
-	projectTitle: "Automatic window",
-	overview: `This window prototype <span class="text-secondary">opens and closes</span> based on
-		weather forecasts, temperature or if it is time to ventilate your room.`,
-	thumbnail: `${base}/images/projects/Automatic_Window.jpeg`,
-	demoLink: `${base}/projects/automatic-window`,
-	technologies: ["Arduino", "Motor control shield"]
-};
+// const vestWithLEDs: Project = {
+// 	completionDate: "01.01.2020",
+// 	projectTitle: "Vest with LEDs",
+// 	overview: `My favourite hardware project. It included six LEDs soldered to cables
+// 		connected to one Arduino Uno, powered by a 9V battery.`,
+// 	thumbnail: `${base}/images/projects/Vest_with_LEDs.jpg`,
+// 	demoLink: `${base}/projects/arduino-vest`,
+// 	technologies: ["Arduino"]
+// };
+// const automaticWindow: Project = {
+// 	completionDate: "02.01.2020",
+// 	projectTitle: "Automatic window",
+// 	overview: `This window prototype <span class="text-secondary">opens and closes</span> based on
+// 		weather forecasts, temperature or if it is time to ventilate your room.`,
+// 	thumbnail: `${base}/images/projects/Automatic_Window.jpeg`,
+// 	demoLink: `${base}/projects/automatic-window`,
+// 	technologies: ["Arduino", "Motor control shield"]
+// };
+// const stockSpreadsheet: Project = {
+// 	completionDate: "05.01.2020",
+// 	projectTitle: "Stock spreadsheet",
+// 	overview: `This spreadsheet imports 8 tables of financial data about a given stock into one worksheet and
+// 		<span class="text-secondary">uses functions to calculate value investing criteria</span>.`,
+// 	thumbnail: `${base}/images/projects/Stock_analysis.jpg`,
+// 	demoLink: `${base}/projects/stockspreadsheet`,
+// 	technologies: ["Excel VBA"]
+// };
 
 const picProjects: Project = {
-	completionDate: "03.01.2020",
 	projectTitle: "Various PIC projects",
 	overview: `In this school project I was programming <span class="text-secondary">five projects and a menu</span>
 		using the PIC18F46K22 microcontroller in C.`,
@@ -39,28 +45,16 @@ const picProjects: Project = {
 };
 
 const portfolio: Project = {
-	completionDate: "04.01.2020",
 	projectTitle: "This website",
 	overview: `I wrote this website myself using sveltekit. It is a <span class="text-secondary">static
-		single page application</span> that makes use of a lot of <span class="text-secondary">
-		web-dev frameworks and GitHub Pages</span>.`,
+		single page application</span> that makes use of a lot of
+		web development frameworks and GitHub Pages.`,
 	thumbnail: `${base}/images/projects/This_Website.jpg`,
 	demoLink: `${base}/projects/this-website`,
 	technologies: ["SvelteKit", "GitHub Pages"]
 };
 
-const stockSpreadsheet: Project = {
-	completionDate: "05.01.2020",
-	projectTitle: "Stock spreadsheet",
-	overview: `This spreadsheet imports 8 tables of financial data about a given stock into one worksheet and
-		<span class="text-secondary">uses functions to calculate value investing criteria</span>.`,
-	thumbnail: `${base}/images/projects/Stock_analysis.jpg`,
-	demoLink: `${base}/projects/stockspreadsheet`,
-	technologies: ["Excel VBA"]
-};
-
 const piHole: Project = {
-	completionDate: "06.01.2020",
 	projectTitle: "PiHole application",
 	overview: `This is a network-wide ad blocker that uses a Raspberry Pi clone to <span class="text-secondary">
 		block ads and malicious links</span> on all devices connected to my home network.`,
@@ -69,13 +63,22 @@ const piHole: Project = {
 	technologies: ["Linux", "Raspberry Pi"]
 };
 
+const ConveyorController: Project = {
+	projectTitle: "Conveyor controller",
+	overview: `This is a system I designed to make it as easy as possible to check quality of conveyor installation as a Bachelor's thesis. In this project I designed a <span class="text-secondary">PCB with a microcontroller</span>, a <span class="text-secondary">mobile application</span> and I made them communicate through <span class="text-secondary>nodeMCU</span>.`,
+	thumbnail: `${base}/images/projects/Conveyor_Controller.jpg`,
+	demoLink: `${base}/projects/conveyor-controller`,
+	technologies: ["PCB Design", "Arduino", "Mobile application", "NodeMCU"]
+};
+
 const projects: Project[] = [
+	ConveyorController,
 	picProjects,
 	portfolio,
-	automaticWindow,
-	piHole,
-	stockSpreadsheet,
-	vestWithLEDs
+	piHole
+	// stockSpreadsheet,
+	// vestWithLEDs,
+	// automaticWindow
 ];
 
 export default projects;
