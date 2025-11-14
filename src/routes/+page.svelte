c1027e37e01932b52e0b73e6cc4a255532d04cb1
davidstrasak <davidstrasak@seznam.cz>
2025-11-14 14:37:15 +0100

    update the title page and start working on an instagram-styled theme for the projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,15 +11,16 @@
 		<img
 			src="{base}/systemancer.svg"
 			alt="systemancer icon"
-			class="h-16 mr-7"
+			class="h-20 mr-7"
 			style="float: left;"
 		/>
 		<strong class="font-systemancer text-primary text-3xl">Systemancer</strong>
 		<p class="italic text-lg">/sis-tuh-man-ser/</p>
 		<p class="text-2xl mx-8 text-left md:text-justify">
-			A determined and passionate engineer and tech enthusiast who likes to solve problems to help
-			people around. Sometimes in <span class="text-secondary">progressive or alternative ways</span
-			>, but all the times <span class="text-secondary">effective</span>.
+			A determined and passionate engineer who likes to solve problems in a systematic manner to
+			help people around. Sometimes in
+			<span class="text-secondary">progressive or alternative ways</span>, but every time
+			<span class="text-secondary">effective</span>.
 		</p>
 		<br />
 	</div>
