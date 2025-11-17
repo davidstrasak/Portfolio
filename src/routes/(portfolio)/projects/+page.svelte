6546527bd942218dfd9a5e32f95730dc9490fe5a
davidstrasak <davidstrasak@seznam.cz>
2025-11-17 22:38:17 +0100

    revert back the changes with the feed view of projects

--- src/routes/(portfolio)/projects/+page.svelte
+++ src/routes/(portfolio)/projects/+page.svelte
@@ -4,12 +4,8 @@
 	import projects from "./projects";
 </script>
 
-<h1 class="block text-4xl text-primary font-systemancer text-center">
-	My recent projects (Lessons Learned)
-</h1>
-<a href="{base}/projects/feed" class="text-xl text-accent underline text-center block"
-	>Want to see this through an instagram-like feed?</a
->
+<h1 class="block text-4xl text-primary font-systemancer text-center">My recent projects</h1>
+<p class="text-xl font-systemancer text-center block">... and the lessons learned from them</p>
 
 {#each projects as project, index}
 	<Project {project} reverse={index % 2 === 0} />
--- src/routes/(portfolio)/projects/feed/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import Project from "$lib/projects/Project.svelte";
-	import { base } from "$app/paths";
-	import projects from "../projects";
-</script>
-
-Work in Progress.
